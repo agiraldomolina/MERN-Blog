a23d4b1c141fe1010b132824cfa33777e722cbbd
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-26 11:37:17 -0500

    add middlewar a, a function to handle errors and a function  catchAsync funtion

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,23 +1,20 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+import { catchAsync } from "../utils/catchAsync.js";
 
-export const signup =async(req,res)=>{
-    try {
-        const {username,email,password} = req.body;
-        if(!username ||!email ||!password || username === "" || email === '' || password === "") return res.json({message:'All fields are required'});
+export const signup = catchAsync(async (req, res, next) => {
+    const {username,email,password} = req.body;
+    if(!username ||!email ||!password || username === "" || email === '' || password === "") return next(errorHandler(400, "All fields are required"));
 
-        const hashedPassword = bcryptjs.hashSync(password, 10)
-    
-        const newUser = new User({
-            username,
-            email,
-            password : hashedPassword
-        });
-    
-        await newUser.save();
-        res.json({message:'User created successfully'});        
-    } catch (error) {
-        res.status(500).json({message:error.message});
-    }
-    // destructuring info from body
-}
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({
+        username,
+        email,
+        password : hashedPassword
+    });
+
+    await newUser.save();
+    res.json({message:'User created successfully'});
+});
