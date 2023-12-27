7697c3e723f2394a779f7b365e6d26d9e505c1c8
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-27 18:00:12 -0500

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,20 +1,23 @@
 
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react'
+import { useDispatch, useSelector} from'react-redux'
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaEye, FaEyeSlash } from'react-icons/fa'
 import {HiMail} from'react-icons/hi'
 
 export default function SignIn() {
   const navigate = useNavigate();
-  const [ errorMessage, setErrorMessage ] = useState(null);
-  const [ loading, setLoading ] = useState(false);
+  const dispatch = useDispatch();
+  const{loading, error:errorMessage} = useSelector(state => state.user)
+  
   const [ formData, setFormData ] = useState({});
   const [ showPassword, setShowPassword ] = useState(true);
-
-  //const PasswordIcon = showPassword? <FaEyeSlash /> : <FaEye />;
-
-  
 
   const handleChange = (event) => {
     // Ths code allow to read new value from the input and keep the values of the others inputs
@@ -22,14 +25,14 @@ export default function SignIn() {
   }
   console.log(formData)
   console.log(formData)
+
   const handleSubmit = async(event) => {
     event.preventDefault();
     if (!formData.email ||!formData.password) {
-      return setErrorMessage('Please fill out all the fields');
+      return dispatch(signInFailure('Please fill out all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       // we need to create a proxy which is specfyed in the vite.config.js file n order this fetch is called
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
@@ -41,14 +44,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        return setErrorMessage('Invalid information');
+        dispatch(signInFailure(data.message));
       }  
-      setLoading(false);
-      if (res.ok) navigate('/');       
+      
+      if (res.ok) {
+        dispatch(signInSuccess(data));  // data is the user object craeted thanks the payload
+        navigate('/')
+      }       
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   }
   return (
