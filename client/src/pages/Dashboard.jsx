d18050105010836a700ce1a21affa925c77ce49a
Alba Giraldo <agiraldomolina@gmail.com>
2023-12-31 00:39:31 -0500

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"
+import DashSideBar from "../components/DashSideBar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get('tab');
+    if (tabFromURL) {
+      setTab(tabFromURL);
+    }
+  }, [location.search])
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* SideBar */}
+        <DashSideBar />
+      </div>
+      {/* Prifile ... */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
   )
 }
