03245d69879b57d291a7516e9f9a88f9f33d4c51
Alba Giraldo <agiraldomolina@gmail.com>
2024-01-02 01:34:36 -0500

    complete user image upload functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -19,7 +19,7 @@ export default function Dashboard() {
         {/* SideBar */}
         <DashSideBar />
       </div>
-      {/* Prifile ... */}
+      {/* Profile ... */}
       {tab === 'profile' && <DashProfile />}
     </div>
   )
