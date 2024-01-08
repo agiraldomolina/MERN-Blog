3adadb33ebf2d0b0b800cc4b43fad392eade0256
Alba Giraldo <agiraldomolina@gmail.com>
2024-01-08 18:40:14 -0500

    add call to action to the post page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import PageLogo from '../assets/PageLogo.png'
 
 export default function CallToAction() {
   return (
-    <div>CallToAction</div>
+    <div id='first' className='flex flex-col justify-center items-center md:flex-row  p-5 border border-teal-500  rounded-tl-3xl rounded-br-3xl text-center gap-4 md:justify-between'>
+        <div className='flex-1 justify-center flex flex-col'>
+            <h2 className='text-2xl'>Embark on a Journey to Wellness!</h2>
+            <p className='text-gray-500 my-2'>If you're passionate about embracing a healthier lifestyle, discovering well-being tips, and nourishing your mind, body, and soul, then you're in the right place.</p>
+
+        </div>
+        <div id='second' className='flex-1'>
+            <div className='p-7 flex justify-center'>
+                <Link to="/"
+                    className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+                    <span
+                        className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'
+                    >  Alba's  </span>
+                    Blog
+                </Link>
+                <img className='h-50 w-40' src={PageLogo} alt="blog image" />
+            </div>
+
+        </div>
+    </div>
   )
 }
